66d6d7083776f06828ac65260a915f267b7bc77b
unknown <nagasaimanoj333@gmail.com>
2022-06-22 06:33:35 +0530

    [Manoj]Adding_Check_Option

--- SnakeAndLadder/SnakeAndLadder/snakeandladder.php
+++ SnakeAndLadder/SnakeAndLadder/snakeandladder.php
@@ -1,14 +1,45 @@
 <?php
 //UC1
- function snakeandladder(){
+class snake
+{
+    const LADDER = 1;
+    const SNAKE = 2;
+    const NOPLAY = 0;
+    public $position = 0;
 
-    $startposition=0;
-  echo "Player at starting Position". $startposition."\n";
-  echo "Rolling a die";
-  $rollDie=rand(1,6);
-  $startposition+=$rollDie;
-  echo "current position is:".$startposition;
- }
- snakeandladder();
-
+ 
+    /*function for palyer roll a die and
+     get number between 1 to 6*/
+    public function snakeandladder()
+    {
+        $startedPosition = 0;
+        echo "Player at starting position:" . $startedPosition . "\n";
+        echo "Rolling a die ";
+        $rollDie = rand(1, 6);
+       // $startedPosition+=$rollDie;
+       // echo "current position is:".$startedPosition;
+        $this->nextMove($rollDie);
+    }
+     //create function to check option for NO Play,Ladder,Snake
+     public function nextMove($rollDie)
+     {
+         $option = rand(0, 2);
+         switch ($option) {
+             case snake::LADDER:
+                 echo " player on Ladder move to next position " . $this->position += $rollDie;
+                 echo "\n";
+                 break;
+             case snake::SNAKE:
+                 echo " player on snake move to previous Position " . $this->position -= $rollDie;
+                 echo "\n";
+                 break;
+             default:
+                 echo " No Play " . $this->position = $this->position;
+                 echo "\n";
+         }
+         echo "Current Position is:" . $this->position . "\n";
+     }
+}
+$obj = new snake();
+$obj->snakeandladder();
 ?>
