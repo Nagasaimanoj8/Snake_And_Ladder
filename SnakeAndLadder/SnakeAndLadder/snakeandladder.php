5e45de616b8794d46c48c0ebd3807dec47e1efbc
unknown <nagasaimanoj333@gmail.com>
2022-06-22 05:52:43 +0530

    [Manoj]Adding_Rollthedice

--- SnakeAndLadder/SnakeAndLadder/snakeandladder.php
+++ SnakeAndLadder/SnakeAndLadder/snakeandladder.php
@@ -3,6 +3,12 @@
  function snakeandladder(){
 
     $startposition=0;
+  echo "Player at starting Position". $startposition."\n";
+  echo "Rolling a die";
+  $rollDie=rand(1,6);
+  $startposition+=$rollDie;
+  echo "current position is:".$startposition;
  }
+ snakeandladder();
 
 ?>
